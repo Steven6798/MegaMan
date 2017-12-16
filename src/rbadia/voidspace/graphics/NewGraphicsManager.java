4cc39ee8d9a2f3fa312edf897e868aae9669a513
Steven A. Ramirez Rosa <Steven.ramirez3@upr.edu>
2017-12-16 15:58:34 -0400

    Added a new bigger platform. Redesign Level3State and now the moving platform bounce. Bug fixes when firing a bigBullet to the left.

--- src/rbadia/voidspace/graphics/NewGraphicsManager.java
+++ src/rbadia/voidspace/graphics/NewGraphicsManager.java
@@ -7,6 +7,7 @@ import java.awt.image.ImageObserver;
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
+import rbadia.voidspace.model.BigPlatform;
 import rbadia.voidspace.model.Boss;
 import rbadia.voidspace.model.Meatball;
 import rbadia.voidspace.model.MegaMan;
@@ -17,10 +18,12 @@ public class NewGraphicsManager extends GraphicsManager{
 	private BufferedImage megaManLImg;
 	private BufferedImage megaFallLImg;
 	private BufferedImage megaFireLImg;
-	private BufferedImage meatball;
-	private BufferedImage bossEnter;
-	private BufferedImage bossFight;
-	private BufferedImage spaghetti;
+	private BufferedImage bigPlatformImg;
+	private BufferedImage meatballImg;
+	private BufferedImage bossEnterImg;
+	private BufferedImage bossFightImg;
+	private BufferedImage spaghettiImg;
+	
 
 	/**
 	 * Creates a new graphics manager and loads the game images.
@@ -31,10 +34,11 @@ public class NewGraphicsManager extends GraphicsManager{
 			this.megaManLImg = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/megaManL.png"));
 			this.megaFallLImg = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/megaFallL.png"));
 			this.megaFireLImg = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/megaFireL.png"));
-			this.meatball = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/Meatball.png"));
-			this.bossEnter = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/BossEnter.png"));
-			this.bossFight = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/BossFight.png"));
-			this.spaghetti = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/Spaghetti.png"));
+			this.meatballImg = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/Meatball.png"));
+			this.bigPlatformImg = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/BigPlatform.png"));
+			this.bossEnterImg = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/BossEnter.png"));
+			this.bossFightImg = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/BossFight.png"));
+			this.spaghettiImg = ImageIO.read(getClass().getResource("/rbadia/voidspace/graphics/Spaghetti.png"));
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "The graphic files are either corrupt or missing.",
 					"VoidSpace - Fatal Error", JOptionPane.ERROR_MESSAGE);
@@ -62,6 +66,10 @@ public class NewGraphicsManager extends GraphicsManager{
 		g2d.drawImage(megaFireLImg, megaMan.x, megaMan.y, observer);	
 	}
 	
+	public void drawBigPlatform (BigPlatform bigPlatform, Graphics2D g2d, ImageObserver observer, int i){
+		g2d.drawImage(bigPlatformImg, bigPlatform.x, bigPlatform.y, observer);	
+	}
+	
 	/**
 	 * Draws Meatball projectile
 	 * @param meatball
@@ -69,7 +77,7 @@ public class NewGraphicsManager extends GraphicsManager{
 	 * @param observer
 	 */
 	public void drawMeatball (Meatball meatball, Graphics2D g2d, ImageObserver observer) {
-		g2d.drawImage(this.meatball, meatball.x, meatball.y, observer);
+		g2d.drawImage(this.meatballImg, meatball.x, meatball.y, observer);
 	}
 	
 	/**
@@ -79,7 +87,7 @@ public class NewGraphicsManager extends GraphicsManager{
 	 * @param observer
 	 */
 	public void drawBossEnter (Boss boss, Graphics2D g2d, ImageObserver observer) {
-		g2d.drawImage(bossEnter, boss.x, boss.y, observer);
+		g2d.drawImage(bossEnterImg, boss.x, boss.y, observer);
 	}
 	
 	/**
@@ -89,7 +97,7 @@ public class NewGraphicsManager extends GraphicsManager{
 	 * @param observer
 	 */
 	public void drawBossFight (Boss boss, Graphics2D g2d, ImageObserver observer) {
-		g2d.drawImage(bossFight, boss.x, boss.y, observer);
+		g2d.drawImage(bossFightImg, boss.x, boss.y, observer);
 	}
 	
 	/**
@@ -99,6 +107,6 @@ public class NewGraphicsManager extends GraphicsManager{
 	 * @param observer
 	 */
 	public void drawSpaghetti(Spaghetti spaghetti, Graphics2D g2d, ImageObserver observer) {
-		g2d.drawImage(this.spaghetti, spaghetti.x, spaghetti.y, observer);
+		g2d.drawImage(this.spaghettiImg, spaghetti.x, spaghetti.y, observer);
 	}
 }
