3c7d50bc12987d38bf826d3997e7205e27051e67
Chief1924 <Chief1924@DESKTOP-0T7CSP7>
2017-12-21 00:24:22 -0400

    Changed Meatball speed (still subject to change). Added new noise, its method and implementation in FinalBoss for some comedy (not perfect, but enough). Deleted old FinalBoss and replaced with new one. New FinalBoss class now functioning.

--- src/rbadia/voidspace/main/FinalBoss.java
+++ src/rbadia/voidspace/main/FinalBoss.java
@@ -11,29 +11,30 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 import rbadia.voidspace.graphics.NewGraphicsManager;
+import rbadia.voidspace.model.Asteroid;
 import rbadia.voidspace.model.BigBullet;
+import rbadia.voidspace.model.BigPlatform;
 import rbadia.voidspace.model.Boss;
 import rbadia.voidspace.model.Bullet;
 import rbadia.voidspace.model.Meatball;
+import rbadia.voidspace.model.NewMegaMan;
 import rbadia.voidspace.model.Platform;
 import rbadia.voidspace.sounds.NewSoundManager;
 
-public class FinalBoss extends Level3State {
-	private static final long serialVersionUID = 1L;
+public class FinalBoss extends Level3State{
+private static final long serialVersionUID = 1L;
 	
 	protected Meatball meatball;
-	protected Boss bossE;
-	protected Boss bossF;
+	protected Boss boss;
+	protected Boss bossHitbox;
+	protected Asteroid deathExplosion;
 	private boolean isBossActive = false;
 	private boolean meatballDidDamage = false;
 	private int hitCounter = 0;
 	
-//	private static AudioInputStream bossStream;
-//	private static File bossSound = new File("/rbadia/voidspace/sounds/Meatball.wav");
-//	private static Clip bossClip;
 	private boolean isMeatballActive = true;
 	
-	public Meatball getMeatball()	{ return meatball; }//Necessary?
+	//public Meatball getMeatball()	{ return meatball; }//Necessary?
 
 	public FinalBoss(int level, NewMainFrame frame, GameStatus status, 
 			NewLevelLogic newGameLogic, InputHandler inputHandler,
@@ -46,14 +47,15 @@ public class FinalBoss extends Level3State {
 		super.doStart();
 		bulletsLeft = new ArrayList<Bullet>();
 		bigBulletsLeft = new ArrayList<BigBullet>();
-		setNumPlatforms(6);
-		setNumBigPlatforms(4);
+		setNumPlatforms(4);
+		setNumBigPlatforms(3);
 		setAsteroidsToDestroy(50);
 		newPlatforms(getNumPlatforms());
 		newBigPlatforms(getNumBigPlatforms());
 		newMeatball(this);
-		newBossEnter(this);
-		newBossFight(this);
+		newBoss(this);
+		newBossHitbox(this);
+		newDeathExplosion(this);
 		NewSoundManager.playToasterSound();
 	}
 	
@@ -63,7 +65,7 @@ public class FinalBoss extends Level3State {
 		setCurrentState(GETTING_READY);
 		getGameLogic().drawGetReady();
 		repaint();
-		LevelLogic.delay(2000);
+		LevelLogic.delay(5000);
 		//Changes music from "menu music" to "ingame music"
 		MegaManMain.audioClip.close();
 		MegaManMain.audioFile = new File("audio/BossBattle.wav");
@@ -81,30 +83,52 @@ public class FinalBoss extends Level3State {
 		}
 	};
 	
+	
 	@Override
 	public void updateScreen() {
-		super.updateScreen();
-//		if (!isBossActive)
-//			drawBossEnter();
-//		else
-//			drawBossFight();
+		Graphics2D g2d = getGraphics2D();
+		GameStatus status = this.getGameStatus();
+		// save original font - for later use
+		if(this.originalFont == null){
+			this.originalFont = g2d.getFont();
+			this.bigFont = originalFont;
+		}
+		clearScreen();
+		drawBackground();
+		drawFloor();
+		drawPlatforms();
+		drawBigPlatforms();
+		drawBoss();
 		drawMeatball();
+		drawMegaMan();
+		drawAsteroid();
+		drawBullets();
+		drawBigBullets();
+		drawBulletsLeft();
+		drawBigBulletsLeft();
+		drawDeath();
 		checkMegaManMeatballCollisions();
-//		if(isBossActive)
-//			checkMegaManBossFCollisions();
-//		else
-//			checkMegaManBossECollisions();
-		checkBulletBossCollisions();
-		checkBigBulletBossCollisions();
+		checkMegaManBossCollisions();
+		if (isBossActive) {
+			checkBulletBossCollisions();
+			checkBigBulletBossCollisions();
+		}
+		else {
+			checkBulletFakeBossCollisions();
+			checkBigBulletFakeBossCollisions();
+		}
+		// update asteroids destroyed (score) label  
+				getNewMainFrame().getDestroyedValueLabel().setText(Long.toString(status.getAsteroidsDestroyed()));
+				// update lives left label
+				getNewMainFrame().getLivesValueLabel().setText(Integer.toString(status.getLivesLeft()));
+				// update level label
+				getNewMainFrame().getLevelValueLabel().setText(Long.toString(status.getLevel()));
 	}
 	
 	
-	
-	
 	protected void drawMeatball() {
-		if (!isBossActive) {
+		if (isBossActive && !isBossDefeated()) {
 			Graphics2D g2d = getGraphics2D();
-			
 			if((meatball.getX() + meatball.getWidth() >  0)) {
 				meatball.translate(-meatball.getSpeed(), 0);
 				getNewGraphicsManager().drawMeatball(meatball, g2d, this);
@@ -131,24 +155,53 @@ public class FinalBoss extends Level3State {
 		}
 	}
 	
-	protected void drawBossEnter() {
-		if (!isBossActive && (bossE.x > this.getWidth() - bossE.width)) {
-			Graphics2D g2d = getGraphics2D();
-			bossE.translate(-bossE.getSpeed(), 0);
-			getNewGraphicsManager().drawBossEnter(bossE, g2d, this);
+	public Boss newBoss(NewLevel1State screen) {
+		int xPos = (int) (screen.getWidth());
+		int yPos = (int) (screen.getHeight() - Boss.HEIGHT - 32);
+		boss = new Boss(xPos, yPos);
+		return boss;
+	}
+	
+	public Boss newBossHitbox(NewLevel1State screen) {
+		int xPos = (int) (screen.getWidth() - Boss.WIDTH + 68);
+		int yPos = (int) (screen.getHeight() - Boss.HEIGHT - 32);
+		bossHitbox = new Boss(xPos, yPos);
+		return bossHitbox;
+	}
+	
+	protected void drawBoss() {
+		Graphics2D g2d = getGraphics2D();
+		if(boss.x > this.getWidth() - Boss.WIDTH) {
+			boss.translate(-boss.getDefaultSpeed(), 0);
+			getNewGraphicsManager().drawBoss(boss, g2d, this);
+			
 		}
 		else {
 			this.isBossActive = true;
-//			Graphics2D g2d = getGraphics2D();
-//			boss.translate(1000000, 1000000);
-//			getNewGraphicsManager().drawBossFight(boss,  g2d,  this);
+			if (!isBossDefeated() && boss.y < this.getWidth()) {
+				getNewGraphicsManager().drawBoss(boss,  g2d,  this);
+			}
+			else if (isBossDefeated() && boss.y < this.getWidth()) {
+				boss.translate(0, boss.getDefaultSpeed());
+				getNewGraphicsManager().drawBoss(boss, g2d, this);
+			}
+			else {
+				this.levelAsteroidsDestroyed += 100;
+			}
 		}
 	}
 	
-	protected void drawBossFight() {
-		if (isBossActive) {
-		Graphics2D g2d = getGraphics2D();
-		getNewGraphicsManager().drawBossFight(bossF,  g2d,  this);
+	public void drawDeath() {
+		if (isBossDefeated()) {
+			Graphics2D g2d = getGraphics2D();
+			getNewGraphicsManager().drawAsteroidExplosion(deathExplosion, g2d, this);
+			if(rand.nextInt(Boss.WIDTH) + boss.x < this.getWidth() &&
+					rand.nextInt(Boss.HEIGHT) + boss.y < 480)
+			deathExplosion.setLocation(rand.nextInt(Boss.WIDTH) + ((int) (this.getWidth() - Boss.WIDTH)),
+										rand.nextInt(boss.y) + ((int) (this.getHeight() - boss.y)));
+			else
+				deathExplosion.setLocation(boss.x, boss.y);
+			NewSoundManager.playDeathExplosionSound();
 		}
 	}
 	
@@ -156,28 +209,24 @@ public class FinalBoss extends Level3State {
 	
 	public Meatball newMeatball(NewLevel1State screen) {
 		int xPos = (int) (screen.getWidth() - Meatball.WIDTH - 50);
-		int yPos = rand.nextInt((int)(screen.getHeight() - Meatball.HEIGHT - 32));
+		int yPos = rand.nextInt(Boss.HEIGHT) + (int)(screen.getHeight() - Boss.HEIGHT - Meatball.HEIGHT);
 		meatball = new Meatball(xPos, yPos);
 		return meatball;
 	}
 	
-	public Boss newBossEnter(NewLevel1State screen) {
-		int xPos = (int) (screen.getWidth());
-		int yPos = (int) (screen.getHeight() - Boss.HEIGHT - 32);
-		bossE = new Boss(xPos, yPos);
-		return bossE;
-	}
-	
-	public Boss newBossFight(NewLevel1State screen) {
-		int xPos = (int) (screen.getWidth() - Boss.WIDTH);
-		int yPos = (int) (screen.getHeight() - Boss.HEIGHT - 32);
-		bossF = new Boss(xPos, yPos);
-		return bossF;
+	public Asteroid newDeathExplosion(NewLevel1State screen) {
+		int xPos = rand.nextInt(Boss.WIDTH) + (int) (screen.getWidth() - Boss.WIDTH);
+		int yPos = rand.nextInt(Boss.HEIGHT) + (int) (screen.getHeight() - Boss.HEIGHT);
+		deathExplosion = new Asteroid(xPos, yPos);
+		return deathExplosion;
 	}
 	
 	
-	
-	
+	@Override
+	public NewMegaMan newMegaMan() {
+		this.megaMan = new NewMegaMan(0, (getHeight() - NewMegaMan.HEIGHT - NewMegaMan.Y_OFFSET) / 2);
+		return megaMan;
+	}
 	
 	@Override
 	protected void drawAsteroid() {
@@ -192,31 +241,25 @@ public class FinalBoss extends Level3State {
 		}
 		return platforms;
 	}
-		
 	
+	@Override
+	public void moveBigPlatform(BigPlatform bigPlatform) {
+		if(bigPlatform.getX() == 0) {
+			platformDirection = 1;
+		}
+		if(bigPlatform.getMaxX() == (getWidth() - Boss.WIDTH)) {
+			platformDirection = -1;
+		}
+		bigPlatform.translate(platformDirection, 0);
+	}
 	
-	
-	protected void isBossDefeated() {
+	protected boolean isBossDefeated() {
 		if (hitCounter >= 50) {
-			levelAsteroidsDestroyed = 50;
+			return true;
 		}
+		else
+			return false;
 	}
-	
-	
-	protected void checkBigBulletBossCollisions() {
-		while (isBossActive) {
-			for(int i = 0; i < bigBullets.size(); i++) {
-				BigBullet bigBullet = bigBullets.get(i);
-				if(bossF.intersects(bigBullet)) {
-					bigBullets.remove(i);
-					hitCounter += 5;
-					damage=0;
-					break;
-				}
-			}
-		}
-	}
-
 	
 	protected void checkMegaManMeatballCollisions() {
 			GameStatus status = getGameStatus();
@@ -226,16 +269,9 @@ public class FinalBoss extends Level3State {
 			}
 	}
 	
-	protected void checkMegaManBossFCollisions() {
+	protected void checkMegaManBossCollisions() {
 		GameStatus status = getGameStatus();
-		if(bossF.intersects(megaMan)) {
-			status.setLivesLeft(status.getLivesLeft() - 1);
-		}
-	}
-	
-	protected void checkMegaManBossECollisions() {
-		GameStatus status = getGameStatus();
-		if(bossE.intersects(megaMan)) {
+		if(boss.intersects(megaMan)) {
 			status.setLivesLeft(status.getLivesLeft() - 1);
 		}
 	}
@@ -243,14 +279,50 @@ public class FinalBoss extends Level3State {
 	protected void checkBulletBossCollisions() {
 		for(int i = 0; i < bullets.size(); i++) {
 			Bullet bullet = bullets.get(i);
-			if(bossF.intersects(bullet)) {
+			if(bossHitbox.intersects(bullet) && this.hitCounter < 50) {
 				// increase asteroids destroyed count
 				bullets.remove(i);
-				hitCounter++;
+				this.hitCounter++;
+				//levelAsteroidsDestroyed++;
+				damage=0;
+				break;
+			}
+		}
+	}
+
+	protected void checkBigBulletBossCollisions() {
+		for(int i = 0; i < bigBullets.size(); i++) {
+			BigBullet bigBullet = bigBullets.get(i);
+			if(bossHitbox.intersects(bigBullet)) {
+				bigBullets.remove(i);
+				this.hitCounter += 5;
 				damage=0;
 				break;
 			}
 		}
 	}
 	
+	protected void checkBulletFakeBossCollisions() {
+		for(int i = 0; i < bullets.size(); i++) {
+			Bullet bullet = bullets.get(i);
+			if(bossHitbox.intersects(bullet) && this.hitCounter < 50) {
+				bullets.remove(i);
+				damage=0;
+				NewSoundManager.playNoSound();
+				break;
+			}
+		}
+	}
+
+	protected void checkBigBulletFakeBossCollisions() {
+		for(int i = 0; i < bigBullets.size(); i++) {
+			BigBullet bigBullet = bigBullets.get(i);
+			if(bossHitbox.intersects(bigBullet)) {
+				bigBullets.remove(i);
+				damage=0;
+				NewSoundManager.playNoSound();
+				break;
+			}
+		}
+	}
 }
--- src/rbadia/voidspace/sounds/NewSoundManager.java
+++ src/rbadia/voidspace/sounds/NewSoundManager.java
@@ -2,6 +2,13 @@ package rbadia.voidspace.sounds;
 
 import java.applet.Applet;
 import java.applet.AudioClip;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import rbadia.voidspace.main.Level1State;
 
@@ -13,6 +20,28 @@ public class NewSoundManager extends SoundManager{
     "/rbadia/voidspace/sounds/Meatball.wav"));
     private static AudioClip toasterSound = Applet.newAudioClip(Level1State.class.getResource(
     "/rbadia/voidspace/sounds/Toasters.wav"));
+    private static AudioClip deathExplosionSound = Applet.newAudioClip(Level1State.class.getResource(
+    	    "/rbadia/voidspace/sounds/shipExplosion.wav"));
+    private static AudioClip noSound = Applet.newAudioClip(Level1State.class.getResource(
+    		"/rbadia/voidspace/sounds/NO.wav"));
+    
+    public static double lengthOfSound() {
+    	try {
+    	File file = new File("src/rbadia/voidspace/sounds/ShipExplosion.wav");
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        AudioFormat format = audioInputStream.getFormat();
+        long frames = audioInputStream.getFrameLength();
+        double durationInSeconds = (frames + 0.0) / format.getFrameRate();
+        return durationInSeconds;
+		} catch (UnsupportedAudioFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return 0.0;
+    }
     
     public static void playMeatballSound() {
     	if(SOUND_ON) {
@@ -29,6 +58,27 @@ public class NewSoundManager extends SoundManager{
     		new Thread(new Runnable() {
     			public void run() {
     				toasterSound.play();
+    			}
+    		}).start();
+    	}
+    }
+    
+    public static void playDeathExplosionSound(){
+		// play sound for asteroid explosions
+    	if(SOUND_ON){
+    		new Thread(new Runnable(){
+    			public void run() {
+    				deathExplosionSound.play();
+    			}
+    		}).start();	
+    	}
+    }
+    
+    public static void playNoSound() {
+    	if(SOUND_ON) {
+    		new Thread (new Runnable() {
+    			public void run() {
+    				noSound.play();
     			}
     		}).start();
     	}
